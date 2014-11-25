3029448f46abf0396bef7762184108cd47f2b599
U-maluch\szymon <szymon@maluch.(none)>
2014-11-25 22:11:57 +0100

    cleaning

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^tango_with_django_project/', include('tango_with_django_project.foo.urls')),
+    # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
 )
