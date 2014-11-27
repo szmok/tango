662afd37bb591d232c3405bda0597fb95ea40e4b
szmok <szrogalski@gmail.com>
2014-11-27 13:44:37 +0100

    added about

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("Rango says: Hello world! <br/> <a href='/rango/about'>About</a> ")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>.")
+
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
+    url(r'^rango/', include('rango.urls')),
+    url(r'^$', include('rango.urls')),
 )
