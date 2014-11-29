cec38fbdc8aa1deacc35280629d450f7cba846dd
U-maluch\szymon <szymon@maluch.(none)>
2014-11-29 19:32:16 +0100

    added media

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+
+from django.conf.urls.static import static # New Import
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,3 +12,10 @@ urlpatterns = patterns('',
     url(r'^rango/', include('rango.urls')),
     url(r'^$', include('rango.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
