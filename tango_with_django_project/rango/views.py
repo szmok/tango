9af2b55d72361d385a156b86e0dc5357a2a394fd
U-maluch\szymon <szymon@maluch.(none)>
2014-11-30 20:05:30 +0100

    admin

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -15,5 +15,8 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>.")
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'rango/about.html', context_dict)
+
+
 
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
 from django.conf.urls.static import static # New Import
@@ -8,14 +8,14 @@ urlpatterns = patterns('',
     # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
     url(r'^$', include('rango.urls')),
 )
-
-if settings.DEBUG:
-    urlpatterns += patterns(
-        'django.views.static',
-        (r'media/(?P<path>.*)',
-        'serve',
-        {'document_root': settings.MEDIA_ROOT}), )
+#
+# if settings.DEBUG:
+#     urlpatterns += patterns(
+#         'django.views.static',
+#         (r'media/(?P<path>.*)',
+#         'serve',
+#         {'document_root': settings.MEDIA_ROOT}), )
