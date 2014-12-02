a30d976dd46bd6fb09489cf300731ec0d354ae6d
U-maluch\szymon <szymon@maluch.(none)>
2014-12-02 21:29:04 +0100

    dynamic template

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __unicode__(self):
-        return self.name
+        return self.name + "(" + str(self.likes) + ";" + str(self.views) + ")"
+
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 
+# Import the Category model
+from rango.models import Category
+
 from django.http import HttpResponse
 
+
 def index(request):
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
